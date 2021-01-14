edf288e2af203a3ee8f650acafaa6f6b59156fb1
Ronaldo Michuy Valera <66487844+Website14-rnmv@users.noreply.github.com>
2021-01-13 19:47:32 -0500

    Add files via upload

---
+++ js/animateScroll.js
@@ -0,0 +1,51 @@
+window.sr = ScrollReveal()
+
+/*---------------------------INICIO DE CABECERA Y NAV--------------------------*/
+
+    sr.reveal('.navbar',{
+        duration : 1500,
+        origin : 'top',
+        distance : '200px'
+
+    });
+
+    
+
+    sr.reveal('.textos',{
+        duration : 2000,
+        origin : 'left',
+        distance : '200px'
+
+    });
+
+    sr.reveal('.imagen_inicio',{
+        duration : 2000,
+        origin : 'right',
+        distance : '200px'
+
+    });
+
+    /*---------------------------INICIO DE WIDGETS INFO--------------------------*/
+
+    sr.reveal('.container_info',{
+        duration : 2000,
+        origin : 'bottom',
+        distance : '200px'
+
+    });
+
+    /*---------------------------INICIO DE COMENTARIOS--------------------------*/
+
+    sr.reveal('.title_coments',{
+        duration : 2000,
+        origin : 'right',
+        distance : '200px'
+
+    });
+
+    sr.reveal('.cajon',{
+        duration : 2000,
+        origin : 'left',
+        distance : '200px'
+
+    });
